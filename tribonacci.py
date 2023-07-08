ed1a1692bbfacd9e707a802ed609d0f7f654bff4
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-07-07 21:03:48 -0300

    .

--- tribonacci.py
+++ tribonacci.py
@@ -3,8 +3,11 @@
 
 def tribonacci(signature, n):
     indice = n
-    lista = range(signature)
-    return print(lista)
+    n1 = signature[0]
+    n2 = signature[1]
+    n3 = signature[2]
 
-ineedherr = [0,1,1]
-tribonacci(ineedherr, 3)
+    tribona = n1+n2+n3
+
+
+tribonacci([0,1,1], 3)
