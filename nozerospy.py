1db6271371d3a45f0e3b3c668dd09d39cd31d6bd
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-07-10 10:31:22 -0300

    .

--- nozerospy.py
+++ nozerospy.py
@@ -1,12 +1,7 @@
-def semzero(n):
+def no_boring_zeros(n):
     n = str(n)
     n = list(n)
-    while n[-1] == '0':
+    while len(n) > 0 and n[-1] == '0':
         n.pop()
     n = ''.join(n)
-    return print(int(n))
-
-
-
-
-semzero(12300320)
+    return int(n)
